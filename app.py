509233f289dd390cdf17d31ee340b54d23e6bb68
Jeomon <jeogeoalukka@gmail.com>
2024-09-30 08:50:25 +0530

    added license

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ load_dotenv()
 
 api_key=environ.get('GROQ_API_KEY')
 llm=ChatGroq('llama-3.1-70b-versatile',api_key,temperature=0)
+
 agent=MetaAgent(llm=llm,tools=[web_search_tool,file_writer_tool],verbose=True)
 input=input("Enter a query: ")
 agent_response=agent.invoke(input)
