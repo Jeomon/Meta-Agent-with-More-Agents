d42b7ee1a5eee89a1446f905a71e1980c6f62d2b
Jeomon <jeogeoalukka@gmail.com>
2024-10-22 10:08:32 +0530

    connected tailwind and shadecn

--- ui/src/main.js
+++ ui/src/main.js
@@ -1,11 +1,8 @@
-import './assets/main.css'
-
+import './assets/index.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-
 app.use(router)
-
 app.mount('#app')
--- ui/vite.config.js
+++ ui/vite.config.js
@@ -3,9 +3,18 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import tailwind from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 
 // https://vite.dev/config/
 export default defineConfig({
+  css:{
+    postcss:{
+      plugins:[
+        tailwind(),autoprefixer()
+      ]
+    }
+  },
   plugins: [
     vue(),
     vueJsx(),
